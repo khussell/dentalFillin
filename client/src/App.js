acf4068575638a34c7c68765831cceab1b900401
Lilu <KHussell@gmail.com>
2019-07-25 09:34:24 -0400

    setting up app.js with routes and components for welcome, login, and sign up

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Welcome from './pages/Welcome'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome</h1>
-    </div>
+    <Router>
+      <Switch>
+        <div className="App">
+          <Route exact path="/" component={Welcome} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signUp" component={SignUp} />
+        </div>
+      </Switch>
+    </Router>
   );
 }
 
