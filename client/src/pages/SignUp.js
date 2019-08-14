fa0a802c06358cb942c91c4c21a11f941b295587
Lilu <KHussell@gmail.com>
2019-08-14 15:17:56 -0400

    working on signup page css

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,7 +1,8 @@
 import React from "react"
-import { Route, Redirect, Link } from "react-router-dom"
+import { Route,  Link } from "react-router-dom"
 import SubSignUp from "../components/SubSignUp"
 import OfficeSignUp from "../components/OfficeSignUp"
+import '../css/signUp.css'
 
 class SignUp extends React.Component {
 
@@ -12,16 +13,22 @@ class SignUp extends React.Component {
        
         
         return (
-            <div>
-                <h1>Sign Up!</h1>
+            <div className="container">
+                <div className="row">
+                <h1 id="signUpTitle" className="col text-center">Sign Up!</h1>
+                </div>
                 <div className="form-group">
-                    <label>Are you a sub or an office?</label>
-                    <Link to={`${this.props.match.url}/sub`} role="button" id="subClick" className="btn btn-link">
+                    <div id="subOrOffice" className="col text-center">
+                    <label>Which are you?</label>
+                    </div>
+                    <div className="col text-center">
+                    <Link to={`${this.props.match.url}/sub`} role="button" id="subClick" className="btn btn-primary">
                         I am a sub
                         </Link>{" "}
-                    <Link to={`${this.props.match.url}/office`} role="button" id="officeClick" className="btn btn-link">
+                    <Link to={`${this.props.match.url}/office`} role="button" id="officeClick" className="btn btn-primary">
                         I am an office
                         </Link>
+                        </div>
                     <Route exact path={`${this.props.match.url}/sub`} render={() => <SubSignUp {...this.props} />} />
                     <Route exact path={`${this.props.match.url}/office`} render={() => <OfficeSignUp {...this.props} />} />
                 </div>
