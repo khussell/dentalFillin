4cad2a0d7406888c57126897d995dfcd516876d0
Lilu <KHussell@gmail.com>
2019-08-21 12:26:25 -0400

    working on heroku deployment

--- server.js
+++ server.js
@@ -47,12 +47,12 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-//mongoose.connect(process.env.MONGODB_URI || "mongodb://user:Flamingo3@ds117535.mlab.com:17535/heroku_lhdwh62t", { useNewUrlParser: true })
-//.then(()=>console.log("connected"))
-//.catch(err => console.log(err))
+ //Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:Flamingo3@ds117535.mlab.com:17535/heroku_lhdwh62t", { useNewUrlParser: true })
+.then(()=>console.log("connected"))
+.catch(err => console.log(err))
 
-mongoose.connect('mongodb://localhost:27017/dentalFillin', {useNewUrlParser: true});
+//mongoose.connect('mongodb://localhost:27017/dentalFillin', {useNewUrlParser: true});
 
 // Start the API server
 app.listen(PORT, function() {
