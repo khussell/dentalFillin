e22fe9ff72437347de5f98f9888690948f22c2d2
Lilu <KHussell@gmail.com>
2019-08-26 18:10:50 -0400

    trying to get heroku dynamic routes to work

--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,8 @@ router.use("/api", apiRoutes);
 
 
 
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+router.get( '*', function(req, res) {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
  });
  
 
